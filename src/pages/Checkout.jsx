6a2d87ef4366e65a951b799929e49ff4d71dd71a
Fedes <fedesanjuan90@gmail.com>
2025-06-08 19:13:05 -0300

    Fix rutas Firebase: import '../firebase/config'

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase/config";
 
+
 function Checkout() {
   const { cart, clearCart } = useContext(CartContext);
   const navigate = useNavigate();
